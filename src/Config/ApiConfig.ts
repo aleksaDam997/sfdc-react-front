8208ba4d1ff041104705dbb458b5e16fcef2c928
DT User6 <aleksa.car1997@gmail.com>
2022-08-29 18:27:44 +0200

    Hope this will work

--- src/Config/ApiConfig.ts
+++ src/Config/ApiConfig.ts
@@ -1,6 +1,6 @@
 export const ApiConfig = {
     // baseUrl: "http://localhost:8080/"
-    http://apps.sf.ues.rs.ba:8080/app/
+    baseUrl: "http://apps.sf.ues.rs.ba:8080/app/"
 }
 
 //"http://apps.sf.ues.rs.ba:8080/sf-collector-server-backend-0.0.1-SNAPSHOT/"
